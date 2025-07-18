fcff063a40ba214db5b94854b850a91431dea2e5
Alexi Peck <alexinicolaspeck@gmail.com>
2025-07-18 16:05:45 -0400

    Add test script

---
+++ src/compile_install_execute_test.ps1
@@ -0,0 +1,20 @@
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Host "This script requires administrative privileges. Please run PowerShell as Administrator."
+    exit
+}
+
+$ProjectDir = "C:\Users\Me\projects\Extension-Base-EDA-BBD"
+$LandisExtensionsDir = "C:\Program Files\LANDIS-II-v8\extensions"
+
+cd $ProjectDir\src\
+dotnet build
+
+Copy-Item -Path "$ProjectDir\src\bin\Debug\netstandard2.0\Landis.Extension.EDA-v3-BBD.dll" -Destination "$LandisExtensionsDir\" -Force
+Copy-Item -Path "$ProjectDir\src\bin\Debug\netstandard2.0\Landis.Extension.EDA-v3-BBD.pdb" -Destination "$LandisExtensionsDir\" -Force
+
+cd $ProjectDir\tests\Core8.0-EDA3.0\
+Write-Output "Executing LANDIS-II..."
+landis-ii-8.cmd .\scenario_CA_coast.txt 2>&1 | Tee-Object -FilePath console-output.txt
+Write-Output "LANDIS-II execution completed."
+
+cd $ProjectDir\src\
